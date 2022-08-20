fa7e83dd662498811e2d1c381c7fc930a21ea28a
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2022-08-20 18:48:34 -0300

    initial commit

---
+++ src/main/java/com/cronolytics/api/CronolyticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.cronolytics.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronolyticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronolyticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cronolytics/api/CronolyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cronolytics.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronolyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
