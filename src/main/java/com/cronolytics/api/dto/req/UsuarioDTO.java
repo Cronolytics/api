031ea4e2a4e12138ddd83c768db8e155045f3f70
isakrm <isaque.monteiror@gmail.com>
2022-09-23 16:09:38 -0300

    config banco azure

--- src/main/java/com/cronolytics/api/dto/req/UsuarioDTO.java
+++ src/main/java/com/cronolytics/api/dto/req/UsuarioDTO.java
@@ -6,9 +6,4 @@ public class UsuarioDTO extends Usuario {
     public UsuarioDTO(String img, String nome, String senha, String email, String cep, String telefone) {
         super(img, nome, senha, email, cep, telefone);
     }
-
-    @Override
-    public String toString() {
-        return "UsuarioDTO{} " + super.toString();
-    }
 }
